d669bfc3e4a355fb31b33441fc9bae36b6aa515c
Erwin Korsten <mw.korsten@gmail.com>
2019-01-22 20:32:24 +0100

    Hover effect added for testing purposes. Might be removed.

--- php/gamecards.php
+++ php/gamecards.php
@@ -1,12 +1,8 @@
 <?php
 
 require_once 'php/db_connect.php';
-$title = '';
-$boxart = '';
-$publisher = '';
-$consoles = '';
 
-	$sql_query = 'SELECT DISTINCT game_name, game_box_art, publisher_name, GROUP_CONCAT(con_short_name SEPARATOR ", ") AS short_name FROM games LEFT OUTER JOIN publisher ON games.publisher_id = publisher.publisher_id LEFT OUTER JOIN game_consoles ON games.game_id = game_consoles.game_id LEFT OUTER JOIN consoles ON game_consoles.console_id = consoles.console_id ORDER BY game_release_year';
+	$sql_query = 'SELECT DISTINCT game_name, game_box_art, game_release_year, publisher_name, GROUP_CONCAT(con_short_name SEPARATOR ", ") AS short_name FROM games LEFT OUTER JOIN publisher ON games.publisher_id = publisher.publisher_id LEFT OUTER JOIN game_consoles ON games.game_id = game_consoles.game_id LEFT OUTER JOIN consoles ON game_consoles.console_id = consoles.console_id ORDER BY game_release_year';
 	$db_result = $conn->query($sql_query);
 
 	foreach ($db_result as $row)
@@ -16,11 +12,13 @@ $consoles = '';
 		$boxart = $row['game_box_art'];
 		$publisher = $row['publisher_name'];
 		$consoles = $row['short_name'];
+		$year = $row['game_release_year'];
 
 		$gamecard = '<div class="grid_item">';
 		$gamecard .= '<div class="card">';
 		$gamecard .= '<h1>' . $title . '</h1>';
 		$gamecard .= '<img src="images/' . $boxart . '" alt="Sonic_the_hedgehog"/>';
+		$gamecard .= '<div class="year">' . $year . '</div>';
 		$gamecard .= '<div class="publisher">Publisher: ' . $publisher . '</div>';
 		$gamecard .= '<div class="consoles">' . $consoles . '</div>';
 		$gamecard .= '</div></div>';
