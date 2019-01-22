a7c8d203c6495ec3b59294087911309cf983b81a
Erwin Korsten <mw.korsten@gmail.com>
2019-01-22 12:01:03 +0100

    Userstory: As an user I want to have a navbar

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang='en-US'>
+
+<head>
+  <meta charset='UTF-8' />
+  <meta name='description' content=' ' />
+  <meta name='keywords' content=' ' />
+  <meta name='author' content='Erwin Korsten' />
+  <meta name='viewport content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+  <title>Retro Run</title>
+	<link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Roboto:400,700&amp;subset=latin-ext" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+	<?php require_once 'php/navbar.php' ?>
+
+
+
+</body>
+
+</html>
---
+++ php/navbar.php
@@ -0,0 +1,17 @@
+<?php
+$url = $_SERVER['HTTP_HOST'];
+$url='http://'.$url.'/week2/';
+?>
+
+	<header>
+		<div class='item'> <h1>Retro <span>Run</span></h1>
+		</div>
+		<div class='item'>
+			<a href='#'>Home</a>
+			<a href='#'>games</a>
+			<a href='#'>consoles</a>
+		</div>
+		<div class='item'>
+			<a href='#'>Login</a>
+		</div>
+	</header>
