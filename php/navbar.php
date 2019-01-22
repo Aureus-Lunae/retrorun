7e87cf5dd510aa2bd01897576754f6d116aa284e
Erwin Korsten <mw.korsten@gmail.com>
2019-01-22 13:14:06 +0100

    userstories: Login link on navbar/Links to items on navbar

--- php/navbar.php
+++ php/navbar.php
@@ -6,12 +6,14 @@ $url='http://'.$url.'/week2/';
 	<header>
 		<div class='item'> <h1>Retro <span>Run</span></h1>
 		</div>
-		<div class='item'>
+		<div class='nav'>
+			<div class='nav_item'>
 			<a href='#'>Home</a>
 			<a href='#'>games</a>
 			<a href='#'>consoles</a>
-		</div>
-		<div class='item'>
-			<a href='#'>Login</a>
+			</div>
+			<div class='nav_item'>
+				<a href='#'>Login</a>
+			</div>
 		</div>
 	</header>
