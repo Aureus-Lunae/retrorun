0b43faa28ffce774c310b5aba50e6593c9395c6b
Erwin Korsten <mw.korsten@gmail.com>
2019-01-22 19:36:39 +0100

    optimized query

--- php/gamecards.php
+++ php/gamecards.php
@@ -5,10 +5,8 @@ $title = '';
 $boxart = '';
 $publisher = '';
 $consoles = '';
-//SELECT DISTINCT game_name, game_box_art, publisher_name, con_short_name FROM games INNER JOIN publisher ON games.publisher_id = publisher.publisher_id INNER JOIN game_consoles ON games.game_id = game_consoles.game_id INNER JOIN consoles ON game_consoles.console_id = game_consoles.console_id
-//SELECT DISTINCT con_name FROM consoles LEFT OUTER JOIN game_consoles ON game_consoles.console_id = consoles.console_id WHERE game_id = '.$
 
-	$sql_query = 'SELECT game_id, game_name, game_box_art, publisher_name FROM games INNER JOIN publisher ON games.publisher_id = publisher.publisher_id';
+	$sql_query = 'SELECT DISTINCT game_name, game_box_art, publisher_name, GROUP_CONCAT(con_short_name SEPARATOR ", ") AS short_name FROM games LEFT OUTER JOIN publisher ON games.publisher_id = publisher.publisher_id LEFT OUTER JOIN game_consoles ON games.game_id = game_consoles.game_id LEFT OUTER JOIN consoles ON game_consoles.console_id = consoles.console_id ORDER BY game_release_year';
 	$db_result = $conn->query($sql_query);
 
 	foreach ($db_result as $row)
@@ -17,32 +15,7 @@ $consoles = '';
 		$title = $row['game_name'];
 		$boxart = $row['game_box_art'];
 		$publisher = $row['publisher_name'];
-		$consoles = '';
-
-		$sql_count = 'SELECT DISTINCT COUNT(con_short_name) AS count FROM consoles LEFT OUTER JOIN game_consoles ON game_consoles.console_id = consoles.console_id WHERE game_id = ' .$row['game_id'];
-
-		$db_count = $conn->query($sql_count);
-
-		foreach ($db_count as $count) {
-			$num_consoles = $count['count'];
-		}
-		//$num_consoles -= 1;
-
-		$sql_query_consoles = 'SELECT DISTINCT con_short_name FROM consoles LEFT OUTER JOIN game_consoles ON game_consoles.console_id = consoles.console_id WHERE game_id = '.$row['game_id'] . ' ORDER BY con_release_year';
-
-		$db_result2 = $conn->query($sql_query_consoles);
-		$i = 0;
-
-		foreach ($db_result2 as $console){
-			$consoles .= $console['con_short_name'];
-
-			if ($i < $num_consoles -1 ) {
-				$consoles .= '; ';
-				$i++;
-			}
-		}
-
-		$conn2 = null;
+		$consoles = $row['short_name'];
 
 		$gamecard = '<div class="grid_item">';
 		$gamecard .= '<div class="card">';
