a1b0ee78233fcaa67a159b0fc0ab52ad94af23f1
Erwin Korsten <mw.korsten@gmail.com>
2019-01-22 14:27:33 +0100

    Userstories: Image for game added/Title for game added

--- index.php
+++ index.php
@@ -1,42 +1,47 @@
 <!DOCTYPE html>
 
-<html lang='en-US'>
+<html lang="en-US">
 
 <head>
-  <meta charset='UTF-8' />
-  <meta name='description' content=' ' />
-  <meta name='keywords' content=' ' />
-  <meta name='author' content='Erwin Korsten' />
-  <meta name='viewport content='width=device-width, initial-scale=1.0, maximum-scale=1.0' />
+  <meta charset="UTF-8" />
+  <meta name="description" content=" " />
+  <meta name="keywords" content=" " />
+  <meta name="author" content="Erwin Korsten" />
+  <meta name="viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   <title>Retro Run</title>
 	<link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Roboto:400,700&amp;subset=latin-ext" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 
 <body>
-	<?php require_once 'php/navbar.php' ?>
+	<?php require_once "php/navbar.php" ?>
 
-		<div class='grid_layout debug'>
+	<section id="games">
+		<div class="grid_layout">
 
 
-			<div class='grid_item debug'>
-				<div class='card debug'>
-					1
+			<div class="grid_item">
+				<div class="card debug">
+					<h1>Title</h1>
+					<img src="images/boxart/sonic_the_hedgehog.png" alt="Sonic_the_hedgehog"/>
+					
+
 				</div>
 			</div>
 
-			<div class='grid_item debug'>
+			<div class="grid_item">
 
 			</div>
-			<div class='grid_item debug'>
+			<div class="grid_item">
 
 			</div>
-			<div class='grid_item debug'>
+			<div class="grid_item">
 
 			</div>
 
 		</div>
 
+	</section>
 
 
 
